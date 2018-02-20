767c26d97ea42be840f2313cf49dabd28cbd85c1
Frédéric Craeymeersch <f.craeymeersch@live.fr>
2018-02-20 13:38:39 +0100

    Design et composants

---
+++ src/components/CardNews.vue
@@ -0,0 +1,147 @@
+<template>
+    <q-card style="margin-bottom:16px; background: #fff">
+        <q-item class="no-margin no-padding" style="padding-top: 15px; overflow: hidden;" v-if="contenu === 'news-high'">
+            <q-card-media style="padding: 0">
+                <div class="img-header">
+                    <img src="/statics/9a920f75-c1cf-4a92-8236-c74a7f928599.jpg" style="width: 100%;">
+                </div>
+                <q-card-title slot="overlay">
+                    <q-item-side avatar="/statics/boy2.svg"/>
+                    <q-item-main>
+                        <div class="col-6 column" style="font-size: 30px; font-weight: bold;">
+                            <span slot="subtitle" style="font-size: 20px;">[{{author}}]</span>
+                            {{title}}
+                        </div>
+                    </q-item-main>
+                </q-card-title>
+            </q-card-media>
+        </q-item>
+        <q-item style="padding-top: 15px;" v-else>
+            <q-item-side avatar="/statics/boy2.svg"/>
+            <q-item-main>
+                <div class="col-6 column" style="font-size: 25px; font-weight: bold;">{{title}}
+                    <span slot="subtitle" style="font-size: 20px;">[{{author}}]</span>
+                </div>
+            </q-item-main>
+        </q-item>
+        <q-item>
+            <p class="text-justify no-margin">{{text}}</p>
+        </q-item>
+        <q-card-actions>
+            <q-item class="date-news">{{date}}</q-item>
+            <q-btn flat>
+                <q-icon name="comment" />
+            </q-btn>
+            <q-btn flat>
+                <q-icon name="share" />
+            </q-btn>
+        </q-card-actions>
+        <!-- Show Video -->
+        <q-card-media v-if="contenu === 'video'">
+            <q-video src="https://www.youtube.com/embed/LYU-8IFcDPw" style=" height: 310px" />
+        </q-card-media>
+        <!-- Show Gallery -->
+        <q-card-media v-if="contenu === 'gallery'">
+            <div class="container">
+                <div class="left">
+                    <div class="row no-margin" style="padding: 0 5px 5px 0 ">
+                        <img style="height: 105px;" src="https://picsum.photos/200">
+                    </div>
+                    <div class="row no-margin">
+                        <img style="height: 105px;" src="https://picsum.photos/200">
+                    </div>
+                </div>
+                <div class="no-margin no-padding right" style="width: auto; height:215px; overflow: hidden">
+                    <img src="https://picsum.photos/1200">
+                </div>
+            </div>
+        </q-card-media>
+
+        <!-- border bottom -->
+        <div class="row justify-around no-margin" style="background: #E8A021; height: 6px">
+        </div>
+    </q-card>
+</template>
+<style>
+    .container {
+        width:100%;
+        display: flex;
+    }
+    .left {
+        float: left;
+    }
+    .right {
+        width:auto;
+        float:right;
+    }
+    .q-item-avatar, .q-item-avatar img{
+        width: 55px;
+        height:55px;
+        border-radius: 0;
+        padding-top:6px;
+    }
+    .date-news {
+        padding: 0px 8px;
+        font-style: italic;
+        font-size: 13px;
+        min-height: 30px;
+        color: #999;
+    }
+    .q-card-media-overlay{
+        /*background: none;*/
+        background: -moz-linear-gradient(bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%); /* FF3.6-15 */
+        background: -webkit-linear-gradient(bottom, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 100%); /* Chrome10-25,Safari5.1-6 */
+        background: linear-gradient(to top, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    }
+    .q-card-media .q-item-side{
+        float: left;
+        padding-right: 10px;
+    }
+    .q-card-media .q-item-main, .q-card-media .q-item-main span {
+        color: #FFF;
+    }
+    .q-item-main span{
+        color: #E8A021;
+    }
+    .q-card-media .q-item-side .q-item-avatar, .q-card-media .q-item-side .q-item-avatar img{
+        padding:0;
+        margin-top:4px;
+    }
+    .q-card-container{
+        padding: 16px 16px 6px 16px;
+    }
+    .q-card-media .img-header{
+        max-height:180px;
+        overflow: hidden;
+
+    }
+</style>
+<script>
+  import {
+    QBtn,
+    QIcon,
+    QItem,
+    QItemSide,
+    QCard,
+    QVideo,
+    QCardTitle,
+    QCardMedia,
+    QCardActions,
+    QItemMain
+  } from 'quasar'
+  export default {
+    props: ['id', 'contenu', 'title', 'author', 'text', 'date'],
+    components: {
+      QVideo,
+      QCard,
+      QCardTitle,
+      QCardMedia,
+      QCardActions,
+      QBtn,
+      QIcon,
+      QItem,
+      QItemSide,
+      QItemMain
+    }
+  }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,132 +14,143 @@
         <div slot="subtitle">Running on Quasar v{{$q.version}}</div>
       </q-toolbar-title>
     </q-toolbar>
-<div class="main" style="width:600px; margin-left:25%">
-<q-card>
-  <q-item>
-    <q-item-side avatar="/statics/boy.svg" />
-    <q-item-main>
-      <q-item-tile label>Title</q-item-tile>
-      <q-item-tile sublabel>Subhead</q-item-tile>
-    </q-item-main>
-  </q-item>
-      <q-collapsible icon="perm_identity" label="Second">
-      <div>
-        Lorem ipsum dolor sit amet...
-      </div>
-    </q-collapsible>
-</q-card>
+<div class="main" style="position: relative; width:540px; margin-left:50%; left: -270px;">
 
-<q-card>
-  <q-card-title>
-        <div class="row items-start items-center">
-        <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
-        <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
-        </div>
-        <q-card-separator />
-  </q-card-title>
-  <q-card-main>
-    <q-item class="row justify-around items-center md-gutter">
-      <div >
-        <p class="col-10">{{lorem}}</p>
-      </div>
-    </q-item>
-    <q-card-separator />
-  </q-card-main>
-  <q-card-media class=" row justify-around">
-    <div class="row">
-    <div>
-      <div class="row">
-        <img style="height: 150px;" src="https://picsum.photos/200">
-      </div>
-      <div class="row">
-        <img style="height: 150px;" src="https://picsum.photos/200">
-      </div>
-    </div>
-    
-    <div>
-      <div>
-        <img style="height: 300px;" src="https://picsum.photos/1200">
-      </div>
-    </div>
-    </div>
-  </q-card-media>
-  <q-card-separator />
-      <q-card-actions>
-        <q-btn flat>
-          <q-icon name="comment" />
-        </q-btn>
-        <q-btn flat>
-          <q-icon name="share" />
-        </q-btn>
-      </q-card-actions>
-</q-card>
+    <!-- implement a card list -->
+    <card-news
+            v-for="item in cardList"
+            v-bind:key="id"
+            v-bind="item"/>
 
-<!-- Video -->
- <q-card>
-
-      <q-card-title>
-        <div class="row items-start items-center">
-        <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
-        <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
-        </div>
-        <q-card-separator />
-      </q-card-title>
-      
-      <q-card-main class="row">
-        <div class="justify-around"> petite vidéo de moi entrain de jouer sous ma douche tranquillement .... et un peut plus de texte pour l'exemple </div>
-      </q-card-main>
-
-      <q-card-media>
-        <div class="row justify-around md-gutter">
-        <q-video class="col-10 " style=" height: 450px" src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"/> 
-       </div>
-      </q-card-media>
-      
-      <q-card-separator />
-      <q-card-actions>
-        <q-btn flat>
-          <q-icon name="comment" />
-        </q-btn>
-        <q-btn flat>
-          <q-icon name="share" />
-        </q-btn>
-      </q-card-actions>
-
-
-
-</q-card>
-<!-- simple text -->
-<q-card>
-      <q-card-title>
-        <div class="row items-start items-center">
-        <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
-        <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
-       </div>
-      </q-card-title>
-      <q-card-separator />
-      
-      <q-card-main>
-        <q-item class="row justify-around items-center md-gutter">
-        <div >
-         <p class="col-10">{{lorem}}</p>
-        </div>
+    <!--<q-card>
+        <q-item>
+            <q-item-side avatar="/statics/boy.svg" />
+            <q-item-main>
+                <q-item-tile label>Title</q-item-tile>
+                <q-item-tile sublabel>Subhead</q-item-tile>
+            </q-item-main>
         </q-item>
-      </q-card-main>
+        <q-item class="row justify-around items-center md-gutter">
+            <div >
+                <p class="col-10">{{lorem}}</p>
+            </div>
+        </q-item>
+        <q-card-actions>
+            <q-btn flat>
+                <q-icon name="comment" />
+            </q-btn>
+            <q-btn flat>
+                <q-icon name="share" />
+            </q-btn>
+        </q-card-actions>
+    </q-card>
 
-      <q-card-separator />
-      <q-card-actions>
-        <q-btn flat>
-          <q-icon name="comment" />
-        </q-btn>
-        <q-btn flat>
-          <q-icon name="share" />
-        </q-btn>
-      </q-card-actions>
-</q-card>
+    <q-card>
+        <q-card-title>
+            <div class="row items-start items-center">
+                <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
+                <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
+            </div>
+        </q-card-title>
+        <q-card-main>
+            <q-item class="row justify-around items-center md-gutter">
+                <div >
+                    <p class="col-10">{{lorem}}</p>
+                </div>
+            </q-item>
+        </q-card-main>
+        <q-card-media class=" row justify-around">
+            <div class="row">
+                <div>
+                    <div class="row">
+                        <img style="height: 150px;" src="https://picsum.photos/200">
+                    </div>
+                    <div class="row">
+                        <img style="height: 150px;" src="https://picsum.photos/200">
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <img style="height: 300px;" src="https://picsum.photos/1200">
+                    </div>
+                </div>
+            </div>
+        </q-card-media>
+        <q-card-actions>
+            <q-btn flat>
+                <q-icon name="comment" />
+            </q-btn>
+            <q-btn flat>
+                <q-icon name="share" />
+            </q-btn>
+        </q-card-actions>
+    </q-card>
+
+    <q-card>
+
+        <q-card-title>
+            <div class="row items-start items-center">
+                <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
+                <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
+            </div>
+        </q-card-title>
+
+        <q-card-main class="row">
+            <div class="justify-around"> petite vidéo de moi entrain de jouer sous ma douche tranquillement .... et un peut plus de texte pour l'exemple </div>
+        </q-card-main>
+
+        <q-card-media>
+            <div class="row justify-around md-gutter">
+                <q-video class="col-10 " style=" height: 450px" src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"/>
+            </div>
+        </q-card-media>
+
+        <q-card-actions>
+            <q-btn flat>
+                <q-icon name="comment" />
+            </q-btn>
+            <q-btn flat>
+                <q-icon name="share" />
+            </q-btn>
+        </q-card-actions>
+
+
+
+    </q-card>
+    <q-card>
+        <q-card-title>
+            <div class="row items-start items-center">
+                <div class="col-1"><q-item-side avatar="/statics/boy.svg" /> </div>
+                <div class="col-6 column" > Title <span slot="subtitle">Subtitle</span></div>
+            </div>
+        </q-card-title>
+
+        <q-card-main>
+            <q-item class="row justify-around items-center md-gutter">
+                <div >
+                    <p class="col-10">{{lorem}}</p>
+                </div>
+            </q-item>
+        </q-card-main>
+
+        <q-card-actions>
+            <q-btn flat>
+                <q-icon name="comment" />
+            </q-btn>
+            <q-btn flat>
+                <q-icon name="share" />
+            </q-btn>
+        </q-card-actions>
+    </q-card>-->
 </div>
   </q-layout>
 </template>
+<style>
+    body{
+        background: #f4f4f4;
+    }
+</style>
 <script>
 import {
   QLayout,
@@ -160,6 +171,7 @@ import {
   QCardMain,
   QItemMain
 } from 'quasar'
+import CardNews from './CardNews.vue'
 
 export default {
   name: 'index',
@@ -180,11 +192,19 @@ export default {
     QListHeader,
     QItem,
     QItemSide,
-    QItemMain
+    QItemMain,
+    CardNews
   },
   data () {
     return {
-      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      cardList: [
+        {id: 0, contenu: 'video', title: 'Faint - Official video', author: 'Linkin\' Park', date: '04 fév. 10h12', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {id: 1, contenu: 'gallery', title: 'Test2', author: 'Author', date: '04 fév. 10h12', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {id: 2, contenu: 'news-high', title: 'Test3', author: 'Author', date: '04 fév. 10h12', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {id: 3, title: 'Test4', author: 'Author', date: '04 fév. 10h12', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {id: 3, contenu: 'video', title: 'Test5', author: 'Author', date: '04 fév. 10h12', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+      ]
     }
   }
 }
